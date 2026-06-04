e88e456f3de0ca2d062f943d04d6ce16f04beee0
Yaniv <yaniv.raines@gmail.com>
2026-06-04 20:56:54 +0200

    fix script.sh

--- shell/script.sh
+++ shell/script.sh
@@ -1,10 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 STDOUT="/dev/stdout"
-LOG_FILE='$STDOUT'
-LOG_MESSAGE='is the date, should log to $STDOUT'
+LOG_FILE="$STDOUT"
+LOG_MESSAGE="is the date, should log to $STDOUT"
 
 # log with timestamp
 log_message() {
-    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> "$LOGFILE"
+    [[ -z "${1:-}" ]] && return 1 # Check if the message is empty
+    printf '[%s] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$1" >> "$LOG_FILE"
 }
 
-log_message $LOG_MESSAGE
+log_message "$LOG_MESSAGE"
